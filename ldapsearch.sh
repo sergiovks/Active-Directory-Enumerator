bc673cd1165ead45c26b72e321b4e02ad98dcc80
SezioS <118485549+sergiovks@users.noreply.github.com>
2023-03-03 19:27:15 +0100

    Update ldapsearch.sh

--- ldapsearch.sh
+++ ldapsearch.sh
@@ -3,13 +3,76 @@
 # Example script to enumerate an Active Directory with ldapsearch
 
 # Set connection variables
-ldap_server="domain-controller.example.com"
-ldap_user="ldapsearch-user"
-domain="example.com"
-password="user-password"
+# ldap_server="domain-controller.example.com"
+# ldap_user="ldapsearch-user"
+# domain="example.com"
+# password="user-password"
 
 # Set search base
 search_base="dc=example,dc=com"
+
+# Define function to display help panel
+display_help() {
+  echo "Usage: $0 [-h] [-lds ldap-server ldap-user] [-d domain] [-p password]"
+  echo "Active Directory enumeration script using ldapsearch"
+  echo " "
+  echo "Options:"
+  echo "-h, --help                show help panel"
+  echo "-lds, --ldap-server       LDAP server to connect to, f. ex: domain-controller.example.com"
+  echo "-ldu, --ldap-user         LDAP user for authentication, f. ex: billy"
+  echo "-d, --domain              AD domain to search, f. ex: example.com"
+  echo "-p, --password            Password for LDAP user, f. ex: 123456789"
+}
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -h|--help)
+    display_help
+    exit 0
+    ;;
+    -lds|--ldap-server)
+    ldap_server="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -ldu|--ldap-user)
+    ldap_user="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -d|--domain)
+    domain="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -p|--password)
+    password="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    echo "Invalid option: $key"
+    display_help
+    exit 1
+    ;;
+esac
+done
+
+# Check that all required arguments are set
+if [[ -z "$ldap_server" || -z "$ldap_user" || -z "$domain" || -z "$password" ]]; then
+    display_help
+    exit 1
+fi
+
+# Determine search base using ldapsearch command
+dn=$(ldapsearch -x -h "$ldap_server" -D "$ldap_user@$domain" -w "$password" -b "" -s base "(objectClass=*)" dn | awk '/dn: / {print $2}')
+
+# Set search base
+search_base="$dn"
 
 # Search for all users
 echo "Users:"
