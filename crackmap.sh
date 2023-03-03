51011e31b152eaa711daf185208bac38adde4cf7
SezioS <118485549+sergiovks@users.noreply.github.com>
2023-03-03 18:42:58 +0100

    Update crackmap.sh

--- crackmap.sh
+++ crackmap.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+usage() {
+  echo "Usage: $0 [-h] [-d DOMAIN] [-dc DOMAIN_CONTROLLER] -u USER [-p PASSWORD] [-N]"
+  echo
+  echo "This script performs several enumeration actions against a Windows Active Directory domain for the SMB protocol using crackmapexec."
+  echo
+  echo "Options:"
+  echo "  -h, --help                                   Show this help message and exit."
+  echo "  -d, --domain DOMAIN                          The domain name to enumerate."
+  echo "  -dc, --domain-controller DOMAIN_CONTROLLER   The hostname or IP address of a domain controller."
+  echo "  -u, --user USER                              The username to use when authenticating to the domain (if not password is provided, user is '')."
+  echo "  -p, --password PASSWORD                      The password to use when authenticating to the domain (if you want to use, if not, user and pass are '')."
+  echo "  -N, --no-pass                                Do not use a password when authenticating to the domain."
+  exit 1
+}
+
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
     key="$1"
@@ -30,6 +45,7 @@ while [[ $# -gt 0 ]]; do
         ;;
         *)    # unknown option
         echo "Unknown option: $1"
+        usage
         exit 1
         ;;
     esac
@@ -47,16 +63,19 @@ fi
 # Set default values for required parameters
 if [ -z "$DC" ]; then
     echo "Domain controller not specified"
+    usage
     exit 1
 fi
 
 if [ -z "$USER" ]; then
     echo "Username not specified"
+    usage
     exit 1
 fi
 
 if [ -z "$DOMAIN" ]; then
     echo "Domain not specified"
+    usage
     exit 1
 fi
 
@@ -93,48 +112,56 @@ if [ "$NOPASS" = true ]; then
     crackmapexec smb $DC $USEROPT $PASSOPT --local-auth --no-pass --no-bruteforce --no-registry --no-wmi --no-mssql --no-snmp --no-winrm -oG computers.gnmap
 else
     crackmapexec smb $DC $USEROPT $PASSOPT --local-auth --no-bruteforce --no-registry --no-wmi --no-mssql --no-snmp --no-winrm -oG computers.gnmap
-    
+fi
+
 # Enumerate shares
 if [ "$NOPASS" = true ]; then
     crackmapexec smb $DC $USEROPT $PASSOPT --local-auth --no-pass --shares > shares.txt
 else
     crackmapexec smb $DC $USEROPT $PASSOPT --local-auth --shares > shares.txt
+fi
 
 # Enumerate domain users with password policy information
 if [ "$NOPASS" = true ]; then
     crackmapexec smb $DC $USEROPT $PASSOPT --no-pass --users --pass-pol > users_with_password_policy_info.txt
 else
     crackmapexec smb $DC $USEROPT $PASSOPT --users --pass-pol > users_with_password_policy_info.txt
-    
+fi
+
 # Enumerate Kerberos tickets
 if [ "$NOPASS" = true ]; then
     crackmapexec smb $DC $USEROPT $PASSOPT --no-pass --kerberos > kerberos_tickets.txt
 else
     crackmapexec smb $DC $USEROPT $PASSOPT --kerberos > kerberos_tickets.txt
+fi
 
 # Dump Kerberos tickets
 if [ "$NOPASS" = true ]; then
     crackmapexec smb $DC $USEROPT $PASSOPT --no-pass --krb5tgs > tickets.kirbi
 else
     crackmapexec smb $DC $USEROPT $PASSOPT --krb5tgs > tickets.kirbi
+fi
 
 # Enumerate computers
 if [ "$NOPASS" = true ]; then
     crackmapexec smb $DC $USEROPT $PASSOPT --no-pass --ping > computers.txt
 else
     crackmapexec smb $DC $USEROPT $PASSOPT --ping > computers.txt
+fi
 
 # Enumerate shares
 if [ "$NOPASS" = true ]; then
     crackmapexec smb $DC $USEROPT $PASSOPT --no-pass --shares > shares.txt
 else
     crackmapexec smb $DC $USEROPT $PASSOPT --shares > shares.txt
+fi
 
 # Enumerate open SMB sessions
 if [ "$NOPASS" = true ]; then
     crackmapexec smb $DC $USEROPT $PASSOPT --no-pass --sessions > sessions.txt
 else
     crackmapexec smb $DC $USEROPT $PASSOPT --sessions > sessions.txt
+fi
 
 # Enumerate local groups on each computer
 if [ "$NOPASS" = true ]; then
@@ -145,3 +172,4 @@ else
     for computer in $(cat computers.txt); do
         crackmapexec smb $computer $USEROPT $PASSOPT --local-groups > "local_groups_$computer.txt"
     done
+fi
